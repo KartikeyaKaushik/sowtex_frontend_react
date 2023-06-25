f82653ade601e85fae41ecc1b27f3b0612fc16c0
Kartikeya Kaushik <Kartikeyakaushik999@email.com>
2023-06-25 23:21:52 +0530

    deployment

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import {BrowserRouter as Router,Route, Routes} from 'react-router-dom';
         <>
             <Router>
             <Routes>
-                <Route path="/home" element={<Home/>}/>
+                <Route path="/" element={<Home/>}/>
             </Routes>
             </Router>
         </>
